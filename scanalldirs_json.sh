a7e9308b7cb79db2dd3715daabbe2c761dc8a77d
Bill Clifford <whcjr46@gmail.com>
2016-12-13 23:00:35 +0000

    Removed some debugging commands

--- scanalldirs_json.sh
+++ scanalldirs_json.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm errors/* keys/* progress/* results/*
-for d in `sudo /google/google-cloud-sdk/bin/gsutil ls gs://isb-cgc-open/NCI-GDC/legacy/TCGA`
+for d in `gsutil ls gs://isb-cgc-open/NCI-GDC/legacy/TCGA`
 do
-    bash -x ./scandir_json.sh $d 1
+    bash ./scandir_json.sh $d 1&
 done
